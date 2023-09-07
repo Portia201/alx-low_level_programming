cd47b192c0b947cf9fbe46662b786816161aad6d
Portia201 <mologadi80@gmail.com>
2023-09-07 01:54:35 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line of words to display
+ * Discription - This program is just a test run to understand 
+ * some features of the C language 
+ * Return: 0
+ */.
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
