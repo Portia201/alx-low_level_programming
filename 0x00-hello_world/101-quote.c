95dfe3715c4e6d514b245f2875c0dff7f1c370a9
Portia201 <mologadi80@gmail.com>
2023-09-07 12:32:21 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - This program makes use of fputs()
  * Description - This program prints to standard output
@@ -8,5 +7,5 @@
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+	return (1);
 }
