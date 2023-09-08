3811751112b82505f93299697c640cc159628023
Portia201 <mologadi80@gmail.com>
2023-09-08 14:10:13 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: eachtime 0 (Success)
+ */
 
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
