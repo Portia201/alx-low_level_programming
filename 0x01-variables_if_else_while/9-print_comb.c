ca122ed125e7be1b70e0a696b6ea09d2337238ce
Portia201 <mologadi80@gmail.com>
2023-09-08 14:59:10 -0700

    printing basic

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - printing comb
+ * Return: eachtime 0 (success)
+ */
+int main(void)
+{
+	char i;
+	
+	int j;
+
+	i = 'a';
+	j = 0;
+	while
+		(j < 10) {
+			putchar(j + '0');
+			j++;
+		}
+	while
+		(i <= 'f') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
