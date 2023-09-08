0b6783f4334464482462e119932672df9c3662bc
Portia201 <mologadi80@gmail.com>
2023-09-08 14:24:10 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print tebahpla
+ * Return: eachtime 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while
+		(i >= 'a') {
+			putchar(i);
+			i--;
+		}
+	putchar('\n');
+	return (0);
+}
