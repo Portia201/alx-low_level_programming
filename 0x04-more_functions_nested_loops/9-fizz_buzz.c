cd52afeaa0302726b0c321ae3027f4b6864321c9
Portia201 <mologadi80@gmail.com>
2023-09-14 23:20:39 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int a;
 for (a = 1; a <= 100; a++)
 {
 if (a % 15 == 0)
-printf("fizzbuzz");
+printf("FizzBuzz");
 else if (a % 3 == 0)
-printf("fizz");
+printf("Fizz");
 else if (a % 5 == 0)
 printf("Buzz");
 else
