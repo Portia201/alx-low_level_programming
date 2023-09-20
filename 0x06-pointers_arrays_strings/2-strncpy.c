0558a1e2364507e5c6f5cea7fdbc16211e54cc71
Portia201 <mologadi80@gmail.com>
2023-09-20 11:18:33 -0700

    strcmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int num_of_bytes;
+int num_of_bytes;
 
-	for (num_of_bytes = 0; num_of_bytes < n && src[num_of_bytes] != '\0'; num_of_bytes++)
-		dest[num_of_bytes] = src[num_of_bytes];
+for (num_of_bytes = 0; num_of_bytes < n && src[num_of_bytes] != '\0'; num_of_bytes++)
+dest[num_of_bytes] = src[num_of_bytes];
 
-	for ( ; num_of_bytes < n; num_of_bytes++)
-		dest[num_of_bytes] = '\0';
+for ( ; num_of_bytes < n; num_of_bytes++)
+dest[num_of_bytes] = '\0';
 
-	return (dest);
+return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - function that compares two strings.
  * @s1: comparing first string
  * @s2: comparing second string
- * Return: value less than 0 if str1 is less than str2, 
+ * Return: value less than 0 if str1 is less than str2,
  * 0 if string is greater compared to other and 0 if they equal.
  */
 int _strcmp(char *s1, char *s2)
