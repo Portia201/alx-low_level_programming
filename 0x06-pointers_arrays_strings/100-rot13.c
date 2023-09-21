84b9ece76b9e34de3336273b6e62bdd66918a3a6
Portia201 <mologadi80@gmail.com>
2023-09-21 05:02:44 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: pointer to string
+ * 
+ * Return: string with leters rot13
+ */
+char *rot13(char *s)
+{
+int a, b;
+
+char i[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; i[b] != '\0'; b++)
+{
+if (s[a] == i[b])
+{
+s[a] = rot[b];
+break;
+}
+}
+}
+return (s);
+}
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,25 +3,26 @@
 /**
  * leet - function that encodes a string into 1337.
  * @str: string to be scanned
- *
  * Return: changed letters
  */
+
 char *leet(char *str);
 {
-	int n, leetSpeak;
+int n, leetSpeak;
 
-	char *c = "aAeEoOtTlL";
-	char *d = "4433007711";
+char *c = "aAeEoOtTlL";
+char *d = "4433007711";
 
-	for (n = 0; str[n] != '\n'; n++)
-	{
-	for (leetSpeak = 0; str[leetSpeak] < 10; leetSpeak++)
-	{
-			if (str[n] == c[leetSpeak])
-			{
-				str[n] = d[leetSpeak];
-			}
-	}
-	}
-	return (str);
+for (n = 0; str[n] != '\n'; n++)
+{
+for (leetSpeak = 0; str[leetSpeak] < 10; leetSpeak++)
+{
+if (str[n] == c[leetSpeak])
+{
+str[n] = d[leetSpeak];
+}
+}
+}
+
+return (str);
 }
