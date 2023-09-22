27fca3accabcd6ef34403db840570cd0e11b6b8a
Portia201 <mologadi80@gmail.com>
2023-09-22 10:31:44 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
+
 	int table;
 
 	while (*s)
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int table;
-	
-	unsigned int sum1 ,sum2;
+	int n;
+	unsigned int sum1, sum2;
 
 	sum1 = 0;
 	sum2 = 0;
 
-	for 
-
+	for (n = 0; n < size; n++)
+	{
+		sum1 += a[(size * n) + n];
+		sum2 += a[(size * (n + 1)) - (n + 1)];
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
