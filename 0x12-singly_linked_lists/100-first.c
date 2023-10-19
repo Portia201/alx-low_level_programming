2addca310a227d5b5d7b879319a2d41665507bd5
Portia201 <mologadi80@gmail.com>
2023-10-19 08:40:43 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence beforevthe main
+ * first - prints a sentence before the main
  * function is executed
  */
-void firs(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
