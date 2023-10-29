67e85f4b696ee20564e630cffca261b1ab6c62fe
Portia201 <mologadi80@gmail.com>
2023-10-29 04:06:26 -0700

    is pime number

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,46 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- *  * wildcmp - compares two strings and returns 1 if the strings
- *   * can be considered identical, otherwise return 0.
- *    * @s1: string to compare to
- *     * @s2: string with wild character
- *      *
- *       * Return: On success 1.
- *        * On error, -1 is returned, and errno is set appropriately.
- *         */
-int wildcmp(char *s1, char *s2)
+ * is_palindrome - checks if string is palindrome
+ * @s: string to reverse
+ * Return: 1 if it is, 0 if its not
+ */
+int is_palindrome(char *s)
 {
-		if (*s1 == '\0' && *s2 == '\0')
-					return (1);
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
 
-			if (*s1 == *s2)
-						return (wildcmp(s1 + 1, s2 + 1));
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
-				if (*s2 == '*')
-						{
-									if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-													return (0);
-											if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-															return (1);
-												}
-
-					return (0);
+/**
+ * check_pal - checks the characters recursively for
+ * @s: string to check
+ * @i: iterator
+ * @len: length to the string
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
