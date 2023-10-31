46abc44f566e9803bb99c85364df533f26fa03e1
Portia201 <mologadi80@gmail.com>
2023-10-31 08:12:36 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,37 +10,36 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int j;
-	int k;
-	int *p = NULL;
-	int l;
-	int ext;
+	int i, j, k, len;
+	char *str;
 
-	l = 0;
-	ext = 0;
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (j = 0; j < ac; j++)
+
+	for (i = 0; i < ac; i++)
 	{
-		for (k = 0; av[j][k] != '\0'; k++)
-		{
-			ext++;
-		}
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
 	}
 
-	p = (char *)malloc(ext + ac + 1 * sizeof(char));
-	if (p == NULL)
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
 		return (NULL);
-	for (j = 0; j < ac; j++)
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
 	{
-		for (k = 0; av[j][k] != '\0'; k++)
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			p[l] = av[j][k];
-			l++;
+			str[k] = av[i][j];
+			k++;
 		}
-		p[l] = '\n';
-		l++;
+		str[k] = '\n';
+		k++;
 	}
-	p[l] = '\0';
-	return (p);
+	
+	return (str);
 }
