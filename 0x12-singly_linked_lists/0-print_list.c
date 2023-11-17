c75c95d1cd8c73ecd84955e00e539389284d4e8f
Portia201 <mologadi80@gmail.com>
2023-11-16 18:04:50 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		s++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
 
-	return (s);
+	return (nodes);
 }
