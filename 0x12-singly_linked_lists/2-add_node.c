43a9fc683584bf1621b2c4e1340b69ae1f3bbaff
Portia201 <mologadi80@gmail.com>
2023-11-16 16:23:16 -0800

    add nodd

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,17 +18,14 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
 	dup = strdup(str);
 	if (dup == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
 	for (len = 0; str[len];)
 		len++;
-
 	new->str = dup;
 	new->len = len;
 	new->next = *head;
