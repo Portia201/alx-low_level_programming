94b775a51bcade9277c59c5c68d5e05304195f50
Portia201 <mologadi80@gmail.com>
2024-02-02 01:06:33 -0800

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+ * hash_djb2 - Hash function, takes a string and turns it into a hash number
+ * @str: the string, a constant value unrepeated in the hash table
+ *
+ * Return: The hash number
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
